158fb22bdd394a77f921f6067d14dbb0414a78ac
Kirill Belyaev <kirill10121415@gmail.com>
2025-06-03 12:40:26 +0300

    edited path to model

--- seq2seq_app.py
+++ seq2seq_app.py
@@ -5,7 +5,7 @@ from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import torch
 
 # Путь к сохранённой модели
-model_path = "./newModel"
+model_path = "./models/last"
 
 # Загружаем модель и токенизатор
 tokenizer = GPT2Tokenizer.from_pretrained(model_path)
